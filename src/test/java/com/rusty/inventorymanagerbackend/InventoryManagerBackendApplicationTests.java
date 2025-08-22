66c7c5ca44a48c9ef0802b8fb5ace3d3013fd0c3
rusty <blaze121304@gmail.com>
2025-08-22 17:59:19 +0900

    2025.08.22 커밋

---
+++ src/test/java/com/rusty/inventorymanagerbackend/InventoryManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusty.inventorymanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
